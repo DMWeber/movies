8c0db3ee451b0a040579072e2087a228aa402f56
Denis Weber <denis.max.weber@gmail.com>
2016-06-28 17:54:14 +0200

    ios project setup

---
+++ ios/movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  movies
+//
+//  Created by Denis Weber on 28.06.16.
+//  Copyright © 2016 kreait. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
